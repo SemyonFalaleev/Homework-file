27b9baaed9ed167283487d80d7ebdfc52fcca68b
SemyonF <Falaleevmay@gmail.com>
2024-02-27 18:19:01 +0700

    Change coock_boock/ and sorted/

--- coock_boock/coock_boock.py
+++ coock_boock/coock_boock.py
@@ -1,9 +1,38 @@
 from pprint import pprint
 def fail_list(fail_name):
+    """Эта функция нужна для перевода всего содержимого файла в list,
+    удаляя все непечатные символы.
+    На ввод принимает имя или путь до файла
+    Выход: список с содержимым файла, за исключением непечатных 
+    символов.
+    """
     with open(fail_name, encoding='utf-8') as recieps:
         return [str_.strip() for str_ in recieps if not str_.isspace()]
     
 def list_dict (list_):
+    '''Эта функция предназначена для создания словаря из листа
+    сформированного функцией fail_list, на основе файла, данные 
+    в котором структурированны по определённым правилам. 
+    Файл должен состоять из одного или нескольких рецептов блюд
+    в таком формате(пример блюда из 3 ингредиентов): 
+    Название блюда
+    3 (колличество ингредиентов в блюде)
+    название ингредиента | число(его колличество) | еденица измерения
+    название ингредиента | число(его колличество) | еденица измерения
+    название ингредиента | число(его колличество) | еденица измерения
+
+    На выходе функция выдаст такой словарь: 
+    {
+  'Название блюда': [
+    {'ingredient_name': 'название ингредиента', 'quantity': число,
+    'measure': 'еденица измерения'},
+    {'ingredient_name': 'название ингредиента', 'quantity': число,
+    'measure': 'еденица измерения'},
+    {'ingredient_name': 'название ингредиента', 'quantity': число,
+    'measure': 'еденица измерения'}
+    ]
+    }
+    '''
     coock_boock = {}
     count = 0
     for indx,str_ in enumerate(list_):
@@ -18,16 +47,34 @@ def list_dict (list_):
             continue
         elif indx == number:
             x = str_.split("|")
-            coock_boock[keys].append({'ingredient_name': x[0], 'quantity': x[1], 'measure': x[2]})
+            coock_boock[keys].append({'ingredient_name': x[0], 'quantity': x[1], 'measure': x[2].strip()})
             keys = 0
             count = 0
             number = 0
         elif indx < number:
             x = str_.split("|")
-            coock_boock[keys].append({'ingredient_name': x[0], 'quantity': x[1], 'measure': x[2]})
+            coock_boock[keys].append({'ingredient_name': x[0], 'quantity': x[1], 'measure': x[2].strip()})
     return coock_boock
 
 def get_shop_list_by_dishes(dishes, person_count):
+    '''Функция предназначена для получения из файла 
+    "coock_boock/recieps.txt" словаря с названием и 
+    колличеством продуктов, для введённых блюд и кол-ва персон
+    На ввод принимает: словарь с названиями блюд имеющихся в файле
+    "coock_boock/recieps.txt", и число персон на которых необходимо
+    приготовить блюда int или str.
+    Пример работы: 
+    Ввод: ['Запеченный картофель', 'Омлет'], 2
+    Вывод: 
+    {
+    'Картофель ': {'measure': 'кг', 'quantity': 2},
+    'Молоко ': {'measure': 'мл', 'quantity': 200},
+    'Помидор ': {'measure': 'шт', 'quantity': 4},
+    'Сыр гауда ': {'measure': 'г', 'quantity': 200},
+    'Чеснок ': {'measure': 'зубч', 'quantity': 6},
+    'Яйцо ': {'measure': 'шт', 'quantity': 4}
+    }
+    '''
     ingredients_list = []
     result_dict = {}
     for dish in dishes:
--- sorted/1.py
+++ sorted/1.py
@@ -1,24 +1,61 @@
 def count_str(name_file):
+    '''Данная функция нужна для подсчёта колличества строк в файле
+    Функция принимает на вход имя файла или его путь в формате str
+    Возвращает число строк в файле начиная от 1 в форамате int
+    Пример:
+    Ввод: "fail1"
+    Вывод: 5
+    '''
     with open(name_file, encoding='utf-8') as file_1:
         return max([count for  count,str_ in enumerate(file_1)])+1
 
 def sorted_file(list_name_file):
+    '''Данная функция предназначаена для сортировки списка с именнами
+    файлов, по колличеству строк в этих файлах.
+    На вход принимает лист, каждый элемент которого предстовляет собой
+    имя файла или путь до него в формате str.
+    Возвращает list с теми же элементами, но отсортированными по
+    колличеству строк в файлах , от меньшего к большему.
+    Пример рабты:
+    Ввод: ["sorted/1.txt", "sorted/2.txt", "sorted/3.txt"] 
+    Вывод: ["sorted/1.txt", "sorted/3.txt", "sorted/2.txt"]
+    '''
     result = {}
-    for indx, name_file in enumerate(list_name_file):
+    for name_file in list_name_file:
         result.setdefault(name_file, count_str(name_file))  
     result = (sorted(result.items(), key = lambda item: item[1]))
     result = [name[0] for name in result]
     return result
 
 def union_file(list_name_file):
-    for indx, name_file in enumerate(sorted(list_name_file)):
+    '''Эта функция преднозначена для объединения N колличества файлов
+    в один, по следующему правилу: 
+    1. Объединяются файлы в том файле, который имеет наименьшее
+    колличество строк
+    2. Файлы записываются в результирующий файл попорядку, от файла с 
+    наименьшим колличеством строк, до файла с наибольшим.
+    3. Содержимое файла предваряеться служебной информацией на 2-х 
+    строках: имя файла и количество строк в нем.
+    Принимает на вход: list каждый объект которого, имя файла или путь
+    до него.
+    Возвращает None
+    '''
+    result = []
+    first_fail = sorted_file(list_name_file)[0]
+    with open(first_fail, encoding="utf-8") as file:
+        result.append(f"{file.name}\n")
+        result.append(f"{str(count_str(first_fail))}\n")
+        result.append(file.read())
+    with open(first_fail, "w", encoding="utf-8") as file:
+        file.writelines(result)
+    result = []
+    for name_file in sorted_file(list_name_file)[1::]:
         with open(name_file, encoding="utf-8") as file:
-            fail_text = file.read()
-    return fail_text
-        # with open(name_file, "w") as file:    
+            result.append(f"{file.name}\n")
+            result.append(f"{str(count_str(name_file))}\n")
+            result.append(f"{file.read()}\n")
+        with open(first_fail, "a", encoding="utf-8") as file:
+            file.writelines(result)
+        result = []
 
-
-
-           
-    
-print(union_file(["sorted/1.txt", "sorted/2.txt", "sorted/3.txt"]))
+union_file(["sorted/1.txt", "sorted/2.txt", "sorted/3.txt"])
